9a0a9ddb43297482998d1624d5a02ccaa85fb224
Mustaf Nuredini <NurediniM@chc.edu>
2021-02-02 10:46:43 -0500

    Tuesday February 2

--- app/src/main/java/com/example/shopper/CreateList.java
+++ app/src/main/java/com/example/shopper/CreateList.java
@@ -1,16 +1,30 @@
 package com.example.shopper;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
 
 public class CreateList extends AppCompatActivity {
+
+    // declare Intent
+    Intent intent;
+
+    // declare EditTexts
+    EditText nameEditText;
+    EditText storeEditText;
+    EditText dateEditText;
+
+    // declare Calendar
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +32,51 @@ public class CreateList extends AppCompatActivity {
         setContentView(R.layout.activity_create_list);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // initialize the EditTexts
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        storeEditText = (EditText) findViewById(R.id.storeEditText);
+        dateEditText = (EditText) findViewById(R.id.dateEditText);
     }
 
-    public void createList(menuItem menuItem){
+    /**
+     * This method further initializes the Action Bar of the activity
+     * It gets the code (XML) in the menu resource file and incorporates it into the Action Bar
+     * @param menu menu resource file for the activity
+     * @return true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_create_list, menu);
+        return true;
+    }
+
+    /**
+     * this method gets called when a menu item in the overflow menu is selected and it
+     * controls what happens when the menu item is selected.
+     * @param item selected menu item in the overflow menu
+     * @return true if menu is selected, else false
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // get the id of menu item selected
+        switch (item.getItemId()){
+            case R.id.action_home :
+                // initialize and Intent for the MainActivity and start it
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_create_list :
+                // initialize and Intent for the MainActivity and start it
+                intent = new Intent(this, CreateList.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    public void createList(MenuItem menuItem){
 
     }
 }
--- app/src/main/java/com/example/shopper/MainActivity.java
+++ app/src/main/java/com/example/shopper/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.shopper;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -15,14 +13,29 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    // declare Intent
+    Intent intent;
+
+    /**
+     *This method initializes the Action Bar and View of the activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // initialize the View and Action Bar of the main activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
+    /**
+     * This method further initializes the Action Bar of the activity
+     * It gets the code (XML) in the menu resource file and incorporates it into the Action Bar
+     * @param menu menu resource file for the activity
+     * @return true
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -30,22 +43,39 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * this method gets called when a menu item in the overflow menu is selected and it
+     * controls what happens when the menu item is selected.
+     * @param item selected menu item in the overflow menu
+     * @return true if menu is selected, else false
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // get the id of menu item selected
+        switch (item.getItemId()){
+            case R.id.action_home :
+                // initialize and Intent for the MainActivity and start it
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_create_list :
+                // initialize and Intent for the MainActivity and start it
+                intent = new Intent(this, CreateList.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * This method gets called when the add Floating Action Button is clicked.
+     * It starts the CreateList Activity
+     * @param view MainActivity view
+     */
     public void openCreateList(View view){
-
+        // initialize and Intent for the MainActivity and start it
+        intent = new Intent(this, CreateList.class);
+        startActivity(intent);
     }
 }
